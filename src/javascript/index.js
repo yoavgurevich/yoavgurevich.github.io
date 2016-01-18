d8491ca736313b9309998007802e76a1ce36d73b
yoavgurevich <ygurevich@ymail.com>
2016-01-18 14:44:37 -0500

    about me, icon, and animations

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,4 @@
-var roman = ['i', 'ii', 'iii', 'iv', 'v'];
+var roman = ['i', 'ii', 'iii', 'iv', 'v', 'vi'];
 
 window.addEventListener('DOMContentLoaded', function() {
     QueryLoader2(document.querySelector("body"), {
@@ -20,14 +20,27 @@ var swiper = new Swiper('.swiper-container', {
     mousewheelControl: true,
     keyboardControl: true,
     direction: 'vertical',
-    resistanceRatio: '0.6'
+    resistanceRatio: '0.6',
+    a11y: true,
+    firstSlideMessage: 'Welcome',
+    lastSlideMessage: 'Contact Info'
 });
 
 $(document).ready(function() {
     $('.carousel').carousel();
     var toastText = " Click on the center of the images \n or their bottom tabs \n to view more information...";
     var $toastContent = $('<span><i id="toastIcon" class="material-icons">info_outline</i>' + toastText + '</span>');
+    var hasFired = false;
+    swiper.once('slideChangeStart', function(){
+        Materialize.fadeInImage('#profile');
+    });
     swiper.once('slideChangeEnd', function(){
-        Materialize.toast($toastContent, 5000, 'pop-up');
+        Materialize.showStaggeredList('#staggered-list');
+    });
+    swiper.on('slideChangeEnd', function(){
+        if(swiper.previousIndex == 1 && !swiper.isBeginning && !hasFired) {
+            Materialize.toast($toastContent, 6000, 'pop-up');
+            hasFired = true;
+        }
     });
 });
