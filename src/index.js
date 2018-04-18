699bdcf57aa500949bca55aa283de2aa5ab4d61e
Yoav Gurevich <ygurevich@ymail.com>
2018-04-18 07:19:13 -0400

    Initial config with HMR complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Testing React Webpack Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React HMR Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
