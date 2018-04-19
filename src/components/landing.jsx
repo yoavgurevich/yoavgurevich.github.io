58ae55e52e6df1e42137cf2edcd30052b972760c
Yoav Gurevich <ygurevich@ymail.com>
2018-04-19 08:45:58 -0400

    Google Fonts will now be imported from URL

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Flexbox from 'flexbox-react';
 
 export default function Landing() {
     return (
-        <h1>This is landing</h1>
+        <Flexbox flexDirection="row" minHeight="100vh" minWidth="100vw">
+            <h1 className='text-center'>This is landing</h1>
+        </Flexbox>
     );
 }
