a4656c121216272e91f71a1ab9b6d7e079b7c286
Yoav Gurevich <ygurevich@ymail.com>
2018-04-20 07:58:27 -0400

    Successfully added scrollable anchor plugin

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { goToAnchor } from 'react-scrollable-anchor'
 
 export default function Landing() {
     return (
-        <div id='video-viewport'>
+        <div id='video-viewport' onClick={() => goToAnchor('engage-people')}>
             <video className="video-bg" autoPlay='true' poster="">
                 <source src="./assets/circuits.mp4" type="video/mp4"/>
             </video>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ScrollableAnchor from 'react-scrollable-anchor'
 import Landing from './components/landing';
+
 import './stylesheets/main.scss'
 
-const title = 'My Minimal React HMR Webpack Babel Setup';
-
 ReactDOM.render(
-  <div>
-      <Landing />
+  <div className='currentBg'>
+      <div>
+        <Landing />
+      </div>
+      <ScrollableAnchor id={'engage-people'}>
+        <div className='fsh vertical-align'>
+          <h1>engage</h1>
+        </div>
+      </ScrollableAnchor>
+      <ScrollableAnchor id={'wellpad'}>
+        <div className='fsh vertical-align'>
+            <h1>Wellpad.io</h1>
+        </div>
+      </ScrollableAnchor>
+      <ScrollableAnchor id={'crkf'}>
+        <div className='fsh vertical-align'>
+          <h1>Chito-Ryu Karate & Fitness</h1>
+        </div>
+      </ScrollableAnchor>
+      <ScrollableAnchor id={'cdot'}>
+        <div className='fsh vertical-align'>
+          <h1>CDOT</h1>
+        </div>
+      </ScrollableAnchor>
+      <ScrollableAnchor id={'contact'}>
+        <div className='fsh vertical-align'>
+          <h1>Contact Me</h1>
+        </div>
+      </ScrollableAnchor>
   </div>,
   document.getElementById('app')
 );
