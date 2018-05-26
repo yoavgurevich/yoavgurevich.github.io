a2261d4b396d3f6dbfc577c0a1bb06db096832fd
Yoav Gurevich <ygurevich@ymail.com>
2018-05-26 11:57:29 -0400

    Added SVG for CDOT logo

--- src/components/partials/cdot.jsx
+++ src/components/partials/cdot.jsx
@@ -14,7 +14,13 @@ export default Watch(class CDOT extends Component {
                 ref={this.props.refsList.cdot}
                 className='fsh vertical-align'>
                 <ScrollButton direction='up' refsList={this.props.refsList} currentElm='cdot' />
-                <h1 className='sanchez'>CDOT</h1>
+                <svg width="650" height="250">
+                    <text x="178" y="140" class="h0 sanchez" fill="white">CD</text>
+                    <circle cx="320" cy="115" r="35" fill="red" />
+                    <circle cx="327" cy="114" r="27" fill="#400000" stroke="white" stroke-width="3" />
+                    <circle cx="330" cy="113" r="15" fill="#EEEEEE" stroke="red" stroke-width="2" />
+                    <text x="359" y="140" class="h0 sanchez" fill="white">T</text>
+                </svg>
                 <ScrollButton direction='down' refsList={this.props.refsList} currentElm='cdot' />
             </section>
         );
