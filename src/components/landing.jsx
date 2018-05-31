9ec9fe144ab27db797005e6ed93c8dc86e8a95d8
Yoav Gurevich <ygurevich@ymail.com>
2018-05-31 08:05:40 -0400

    Added conditional rendering for landing content

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,35 +4,49 @@ import ScrollButton from './scrollButton';
 export default class Landing extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            loadContent: false
+        }
+
+        this.handlePlay = this.handlePlay.bind(this);
+    }
+
+    handlePlay(e) {
+        setTimeout(() => this.setState({loadContent: true}), 250);
     }
 
     render() {
+        const loadedContent = this.state.loadContent ? (
+            <div className='landing-content vertical-align'>
+                <h1 className='text-center'>Yoav Gurevich</h1>
+                <div className='landing-img'></div>
+                <blockquote>
+                    <h5>
+                        <p>
+                            Seneca College computer programming & analysis graduate of 2015. Studied mixology and wine council studies before that.
+                            Dabbled in audio engineering and production before that. Guitar player for nigh on 12 years now, and a big believer
+                            in the harmony between opposite schools of knowledge and experience.
+                        </p>
+                        <p>
+                            The project overviews below highlight my humble contributions to a few different use cases. 
+                            If there are two things I've learned about the development process so far, it's that
+                            it could be as personal and unique as songwriting, and that passion is <i>very</i> contagious.
+                        </p>
+                    </h5>
+                    <ScrollButton direction='down' refsList={this.props.refsList} currentElm='landing' />
+                </blockquote>
+            </div>
+        ) : <div></div>;
+
         return (
             <div ref={this.props.refsList.landing}>
                 <div id='video-viewport'>
-                    <video className="video-bg" autoPlay='true' muted='true'>
+                    <video onPlay={this.handlePlay} className="video-bg" autoPlay='true' muted='true'>
                         <source src="/assets/ink4k_edit.mp4" type="video/mp4"/>
                     </video>
                 </div>
-                <div className='landing-content vertical-align'>
-                    <h1 className='text-center'>Yoav Gurevich</h1>
-                    <div className='landing-img'></div>
-                    <blockquote>
-                        <h5>
-                            <p>
-                                Seneca College computer programming & analysis graduate of 2015. Studied mixology and wine council studies before that.
-                                Dabbled in audio engineering and production before that. Guitar player for nigh on 12 years now, and a big believer
-                                in the harmony between opposite schools of knowledge and experience.
-                            </p>
-                            <p>
-                                The project overviews below highlight my humble contributions to a few different use cases. 
-                                If there are two things I've learned about the development process so far, it's that
-                                it could be as personal and unique as songwriting, and that passion is <i>very</i> contagious.
-                            </p>
-                        </h5>
-                        <ScrollButton direction='down' refsList={this.props.refsList} currentElm='landing' />
-                    </blockquote>
-                </div>
+                {loadedContent}
             </div>
         );
     }
