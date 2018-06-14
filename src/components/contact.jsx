0e846b9b3d2a315bade1fdc1273e28840a808531
Yoav Gurevich <ygurevich@ymail.com>
2018-06-14 08:47:12 -0400

    Contact page background changed (again)

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,27 +13,26 @@ export default function ContactMe(props) {
                 <h2>Stay in touch...</h2>
                 <ul>
                     <li>
-                        <h2>
-                            <a href='//ca.linkedin.com/in/yoav-gurevich-42415886' target='_blank'>
-                                <i className='fa fa-linkedin-square'>&nbsp;</i>
-                            </a>
-                            <a href='//twitter.com/ygdot14' target='_blank'>
-                                <i className='fa fa-twitter'>&nbsp;</i>
-                            </a>
-                            <a href='//github.com/yoavgurevich' target='_blank'>
-                                <i className='fa fa-github'>&nbsp;</i>
-                            </a>
-                        </h2>
+                        <a href='//ca.linkedin.com/in/yoav-gurevich-42415886' target='_blank'>
+                            <i className='fa fa-linkedin-square'>&nbsp;</i>
+                        </a>
+                        <a href='//twitter.com/ygdot14' target='_blank'>
+                            <i className='fa fa-twitter'>&nbsp;</i>
+                        </a>
+                        <a href='//github.com/yoavgurevich' target='_blank'>
+                            <i className='fa fa-github'>&nbsp;</i>
+                        </a>
                     </li>
                 </ul>
                 <br />
-                <h4 className='margin-bottom-5'>This portfolio was made possible thanks to these great projects:</h4>
+                <h5 className='margin-bottom-5'>This portfolio was made possible thanks to these great projects:</h5>
                 <section className='vendor-imgs'>
                     <a href='//reactjs.org/' target='_blank'><img src='/assets/react.png' alt='React.js' /></a>
                     <a href='//webpack.js.org' target='_blank'><img src='/assets/webpack.png' alt='Webpack' /></a>
                     <a href='//sass-lang.com' target='_blank'><img src='/assets/sass.png' alt='Sass' /></a>
                 </section>
-                <img src='/assets/rotary.svg' className='rotary' />
+                <br />
+                <br />
                 <br />
                 <small>Landing page clip courtesy of <a href='//mitchmartinez.com/' target='_blank'>Mitch Martinez</a><sup>&trade;</sup>. Stock footage license agreement available upon request.</small>
             </main>
