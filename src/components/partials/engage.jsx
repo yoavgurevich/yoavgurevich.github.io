38cf05c4c78ef2b4b75646158814d9be8cfdfbb6
Yoav Gurevich <ygurevich@ymail.com>
2018-12-19 08:24:36 -0500

    Added first slide content

--- src/components/partials/engage.jsx
+++ src/components/partials/engage.jsx
@@ -8,10 +8,28 @@ export default class Engage extends Component {
  
     render () {
         let resolvedContent = this.props.currentPage == 0 ? (
-            <div className='fsh vertical-align'>
-                <ScrollButton direction='up' refsList={this.props.refsList} resolveBg={this.props.resolveBg} />
-                <h1>engage</h1>
-                <ScrollButton direction='down' refsList={this.props.refsList} resolveBg={this.props.resolveBg} />
+            <div className='fsh grid-container'>
+                <div className="grid-header">
+                    <ScrollButton direction='up' refsList={this.props.refsList} />
+                </div>
+                <div className="grid-content">
+                    <h1 className='text-center slide-left'>engage</h1>
+                    <div className='fade-in'>
+                        <img src='https://www.engagepeople.com/img/Podium_logo.svg' alt='Podium Logo'></img>
+                        <img src='https://www.engagepeople.com/img/LRG_logo.svg' alt='LRG Logo'></img>
+                        <p>
+                            Spending time on two of the loyalty powerhouse's flagship products Podium & LRG (Axs)
+                            has helped hone a vast array of full stack web development skills. 
+                            <br /><br />
+                            Catering to enterprise clientele
+                            seeking robust, modular, and secure platforms requires consistent attention to detail and processes 
+                            that build on iterative improvement.
+                        </p>
+                    </div>
+                </div>
+                <div className="grid-footer">
+                    <ScrollButton direction='down' refsList={this.props.refsList} />
+                </div>
             </div>
         ) : <div></div>;
 
