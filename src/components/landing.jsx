1d2207c37d96a3d2ff72957c9157def3219f8ff0
Yoav Gurevich <ygurevich@ymail.com>
2019-01-05 14:21:53 -0500

    Updated intro page pic timings

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -6,7 +6,6 @@ export default class Landing extends Component {
         super(props);
 
         this.state = {
-            loadContent: false,
             currentPic: 'yg1',
             currentIterator: 1
         }
@@ -15,53 +14,25 @@ export default class Landing extends Component {
     }
 
     componentDidMount() {
-        if (!this.state.loadContent) {
-            setTimeout(() => {
-                this.setState({loadContent: true});
-                this.cyclePics();            
-            }, 250);
-        }
+        this.cyclePics();
     }
 
     cyclePics() {
-        const resolvedDelay = this.state.currentIterator === 1 ? 5000 : 1200;
-
         if (this.state.currentPic !== 'yg5') {
             setTimeout(() => {
-                this.setState((prevState, props) => {
+                this.setState((prevState) => {
                     return {
                         currentPic: `yg${prevState.currentIterator}`,
                         currentIterator: prevState.currentIterator + 1
                     }
                 });
+
                 this.cyclePics();
-            }, resolvedDelay);
+            }, 1200);
         }
     }
 
     render() {
-        const loadedContent = this.state.loadContent ? (
-            <div className='landing-content vertical-align'>
-                <h1 className='text-center'>Yoav Gurevich</h1>
-                <div className={'landing-img slide-in-bck-center ' + this.state.currentPic}></div>
-                <blockquote>
-                    <h5>
-                        <p>
-                            Seneca College computer programming & analysis graduate of 2015. Studied mixology and wine council studies before that.
-                            Dabbled in audio engineering and production before that. Guitar player for nigh on 12 years now, and a big believer
-                            in the harmony between opposite schools of knowledge and experience.
-                        </p>
-                        <p>
-                            The project overviews below highlight my humble contributions to a few different use cases. 
-                            If there are two things I've learned about the development process so far, it's that
-                            it could be as personal and unique as songwriting, and that passion is <i>very</i> contagious.
-                        </p>
-                    </h5>
-                </blockquote>
-                <ScrollButton direction='down' refsList={this.props.refsList} currentElm='landing'  resolveBg={this.props.resolveBg} />
-            </div>
-        ) : <div></div>;
-
         return (
             <div className='fsh' ref={this.props.refsList.landing}>
                 {/* <div id='video-viewport'>
@@ -69,7 +40,25 @@ export default class Landing extends Component {
                         <source src="/assets/ink4k_edit.mp4" type="video/mp4"/>
                     </video>
                 </div> */}
-                {loadedContent}
+                <div className='landing-content vertical-align'>
+                    <h1 className='text-center'>Yoav Gurevich</h1>
+                    <div className={'landing-img slide-in-bck-center ' + this.state.currentPic}></div>
+                    <blockquote>
+                        <h5>
+                            <p>
+                                Seneca College computer programming & analysis graduate of 2015. Studied mixology and wine council studies before that.
+                                Dabbled in audio engineering and production before that. Guitar player for nigh on 12 years now, and a big believer
+                                in the harmony between opposite schools of knowledge and experience.
+                            </p>
+                            <p>
+                                The project overviews below highlight my humble contributions to a few different use cases. 
+                                If there are two things I've learned about the development process so far, it's that
+                                it could be as personal and unique as songwriting, and that passion is <i>very</i> contagious.
+                            </p>
+                        </h5>
+                    </blockquote>
+                    <ScrollButton direction='down' refsList={this.props.refsList} currentElm='landing'  resolveBg={this.props.resolveBg} />
+                </div>
             </div>
         );
     }
