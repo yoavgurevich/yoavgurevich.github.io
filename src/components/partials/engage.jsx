738c0bf9cc086aaf348a6892c5498acd91e1947d
Yoav Gurevich <ygurevich@ymail.com>
2019-01-07 08:15:00 -0500

    initial layout occurs only on first time now

--- src/components/partials/engage.jsx
+++ src/components/partials/engage.jsx
@@ -4,6 +4,16 @@ import ScrollButton from '../scrollButton';
 export default class Engage extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            hasAnimated: false
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.props.currentBg == 'indianred' && !this.state.hasAnimated) {
+            this.setState({ hasAnimated: true });
+        }
     }
  
     render () {
@@ -12,9 +22,9 @@ export default class Engage extends Component {
                 <div className="grid-header">
                     <ScrollButton direction='up' refsList={this.props.refsList} />
                 </div>
-                <div className={this.props.currentBg == 'indianred' ? 'grid-content' : 'grid-content col-1'}>
-                    <h1 className={this.props.currentBg == 'indianred' ? 'text-center slide-left' : 'text-center'}>engage</h1>
-                    {this.props.currentBg == 'indianred' ? (
+                <div className={this.props.currentBg == 'indianred' || this.state.hasAnimated ? 'grid-content' : 'grid-content col-1'}>
+                    <h1 className={this.props.currentBg == 'indianred' || this.state.hasAnimated ? 'text-center slide-left' : 'text-center'}>engage</h1>
+                    {this.props.currentBg == 'indianred' || this.state.hasAnimated ? (
                         <div className='fade-in'>
                         <a className='img-link' href='https://www.engagepeople.com/podium.html' rel='noopener noreferrer' target='_blank'><img src='https://www.engagepeople.com/img/Podium_logo.svg' alt='Podium Logo'></img></a>
                         <a className='img-link' href='https://www.engagepeople.com/lrg.html' rel='noopener noreferrer' target='_blank'><img src='https://www.engagepeople.com/img/LRG_logo.svg' alt='LRG Logo'></img></a>
