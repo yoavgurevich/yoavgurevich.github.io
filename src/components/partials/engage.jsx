a7603d0e5ea98b6c18e5c19b358df6bb7ea91229
Yoav Gurevich <ygurevich@ymail.com>
2019-01-08 08:37:02 -0500

    initial re-addition of old slide media

--- src/components/partials/engage.jsx
+++ src/components/partials/engage.jsx
@@ -26,17 +26,17 @@ export default class Engage extends Component {
                     <h1 className={this.props.currentBg == 'indianred' || this.state.hasAnimated ? 'text-center slide-left' : 'text-center'}>engage</h1>
                     {this.props.currentBg == 'indianred' || this.state.hasAnimated ? (
                         <div className='fade-in'>
-                        <a className='img-link' href='https://www.engagepeople.com/podium.html' rel='noopener noreferrer' target='_blank'><img src='https://www.engagepeople.com/img/Podium_logo.svg' alt='Podium Logo'></img></a>
-                        <a className='img-link' href='https://www.engagepeople.com/lrg.html' rel='noopener noreferrer' target='_blank'><img src='https://www.engagepeople.com/img/LRG_logo.svg' alt='LRG Logo'></img></a>
-                        <p>
-                            Spending time on two of the loyalty powerhouse's flagship products Podium & LRG (Axs)
-                            has helped hone a vast array of full stack web development skills. 
-                            <br /><br />
-                            Catering to enterprise clientele
-                            seeking robust, modular, and secure platforms requires consistent attention to detail and processes 
-                            that build on iterative improvement.
-                        </p>
-                    </div>
+                            <a className='img-link' href='https://www.engagepeople.com/podium.html' rel='noopener noreferrer' target='_blank'><img src='https://www.engagepeople.com/img/Podium_logo.svg' alt='Podium Logo'></img></a>
+                            <a className='img-link' href='https://www.engagepeople.com/lrg.html' rel='noopener noreferrer' target='_blank'><img src='https://www.engagepeople.com/img/LRG_logo.svg' alt='LRG Logo'></img></a>
+                            <p>
+                                Spending time on two of the loyalty powerhouse's flagship products Podium & LRG (Axs)
+                                has helped hone a vast array of full stack web development skills. 
+                                <br /><br />
+                                Catering to enterprise clientele
+                                seeking robust, modular, and secure platforms requires consistent attention to detail and processes 
+                                that build on iterative improvement.
+                            </p>
+                        </div>
                     ) : <div></div>}
                 </div>
                 <div className="grid-footer">
--- src/components/partials/wellpad.jsx
+++ src/components/partials/wellpad.jsx
@@ -8,21 +8,33 @@ export default class Wellpad extends Component {
  
     render () {
         let resolvedContent = this.props.currentPage == 1 ? (
-            <div className='fsh grid-container'>
+            <div className='fsh grid-container wellpad-bg'>
                 <div className="grid-header">
                     <ScrollButton direction='up' refsList={this.props.refsList} />
                 </div>
-                <div className="grid-content">
+                <div className="grid-content margin-bottom-6">
                     <h1 className='rouge-script h0 text-center slide-left'>Wellpad.io</h1>
-                    <p className='fade-in'>
-                        This iPad-centric solution was commissioned in order to ameliorate an older system used for managing a health clinic's
-                        patient and staff data. It provides an intuitive survey platform, as well as an administrative interface for viewing current information and trends
-                        in both new and returning patients.
-                        <br /><br />
-                        My primary role for this project was front-end view and component implementation for the patient-intake and follow-up surveys.
-                        Design mock-ups were issued out by team lead <a href="https://www.linkedin.com/in/leesaynor/" rel="noopener noreferrer" target='_blank'>Lee Saynor</a>, and I was to try and bring them to life and make them usable the best of my ability.
-                        Lead Developer <a href="https://twitter.com/tabakd_" rel="noopener noreferrer" target='_blank'>Daniel Tabak</a> maintained a unique and frenetic pace throughout every milestone.
-                    </p>
+                    <div className='fade-in'>
+                        <div className='img-group'>
+                            <img src='https://i.imgur.com/CIsoSBG.png' alt='wellpad image 1'></img>
+                            <img src='https://i.imgur.com/H9bpbJm.png' alt='wellpad image 2'></img>
+                            <img src='https://i.imgur.com/59BmDKw.png' alt='wellpad image 3'></img>
+                        </div>
+                        <p>
+                            This iPad-centric solution was commissioned in order to ameliorate an older system used for managing a health clinic's
+                            patient and staff data. It provides an intuitive survey platform, as well as an administrative interface for viewing current information and trends
+                            in both new and returning patients.
+                            <br /><br />
+                            My primary role for this project was front-end view and component implementation for the patient-intake and follow-up surveys.
+                            Design mock-ups were issued out by team lead <a href="https://www.linkedin.com/in/leesaynor/" rel="noopener noreferrer" target='_blank'>Lee Saynor</a>, and I was to try and bring them to life and make them usable the best of my ability.
+                            Lead Developer <a href="https://twitter.com/tabakd_" rel="noopener noreferrer" target='_blank'>Daniel Tabak</a> maintained a unique and frenetic pace throughout every milestone.
+                        </p>
+                        <div className='img-group'>
+                            <img src='https://i.imgur.com/OAye111.png' alt='wellpad image 4'></img>
+                            <img src='https://i.imgur.com/9uZQe3K.png' alt='wellpad image 5'></img>
+                            <img src='https://i.imgur.com/osl2TbD.png' alt='wellpad image 6'></img>
+                        </div>
+                    </div>
                 </div>
                 <div className="grid-footer">
                     <ScrollButton direction='down' refsList={this.props.refsList} />
