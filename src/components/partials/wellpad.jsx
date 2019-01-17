1f314add82d609a8f2c88709e5f3582fbb1feecc
Yoav Gurevich <ygurevich@ymail.com>
2019-01-17 08:34:18 -0500

    javascript for zoom functionality added

--- src/components/partials/wellpad.jsx
+++ src/components/partials/wellpad.jsx
@@ -4,6 +4,24 @@ import ScrollButton from '../scrollButton';
 export default class Wellpad extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            focusMode: null,
+            zoomElement: null
+        }
+
+        this.toggleFocusMode = this.toggleFocusMode.bind(this);
+    }
+
+    toggleFocusMode(e) {
+        let elemId = e.target.id;
+
+        this.setState((currentState) => {
+            return {
+                focusMode: currentState.focusMode === 'zoomed-in' ? '' : 'zoomed-in',
+                zoomElement: elemId
+            }
+        });
     }
  
     render () {
@@ -14,11 +32,11 @@ export default class Wellpad extends Component {
                 </div>
                 <div className="grid-content margin-bottom-40">
                     <h1 className='rouge-script h0 text-center slide-left'>Wellpad.io</h1>
-                    <div className='fade-in'>
+                    <div className={'fade-in ' + this.state.focusMode}>
                         <div className='img-group'>
-                            <img src='https://i.imgur.com/CIsoSBG.png' alt='wellpad image 1'></img>
-                            <img src='https://i.imgur.com/H9bpbJm.png' alt='wellpad image 2'></img>
-                            <img src='https://i.imgur.com/59BmDKw.png' alt='wellpad image 3'></img>
+                            <img id='zoom-img-1' className={this.state.zoomElement == 'zoom-img-1' ? 'current-zoom' : ''} src='https://i.imgur.com/CIsoSBG.png' alt='wellpad image 1' onClick={this.toggleFocusMode}></img>
+                            <img id='zoom-img-2' className={this.state.zoomElement == 'zoom-img-2' ? 'current-zoom' : ''} src='https://i.imgur.com/H9bpbJm.png' alt='wellpad image 2' onClick={this.toggleFocusMode}></img>
+                            <img id='zoom-img-3' className={this.state.zoomElement == 'zoom-img-3' ? 'current-zoom' : ''} src='https://i.imgur.com/59BmDKw.png' alt='wellpad image 3' onClick={this.toggleFocusMode}></img>
                         </div>
                         <p>
                             This iPad-centric solution was commissioned in order to ameliorate an older system used for managing a health clinic's
@@ -30,9 +48,9 @@ export default class Wellpad extends Component {
                             Lead Developer <a href="https://twitter.com/tabakd_" rel="noopener noreferrer" target='_blank'>Daniel Tabak</a> maintained a unique and frenetic pace throughout every milestone.
                         </p>
                         <div className='img-group'>
-                            <img src='https://i.imgur.com/OAye111.png' alt='wellpad image 4'></img>
-                            <img src='https://i.imgur.com/9uZQe3K.png' alt='wellpad image 5'></img>
-                            <img src='https://i.imgur.com/osl2TbD.png' alt='wellpad image 6'></img>
+                            <img id='zoom-img-4' className={this.state.zoomElement == 'zoom-img-4' ? 'current-zoom' : ''} src='https://i.imgur.com/OAye111.png' alt='wellpad image 4' onClick={this.toggleFocusMode}></img>
+                            <img id='zoom-img-5' className={this.state.zoomElement == 'zoom-img-5' ? 'current-zoom' : ''} src='https://i.imgur.com/9uZQe3K.png' alt='wellpad image 5' onClick={this.toggleFocusMode}></img>
+                            <img id='zoom-img-6' className={this.state.zoomElement == 'zoom-img-6' ? 'current-zoom' : ''} src='https://i.imgur.com/osl2TbD.png' alt='wellpad image 6' onClick={this.toggleFocusMode}></img>
                         </div>
                     </div>
                 </div>
