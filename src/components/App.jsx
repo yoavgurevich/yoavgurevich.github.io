8344b9ef92c84d17c9a19d17530989d98438dc7f
Yoav Gurevich <ygurevich@ymail.com>
2020-05-24 12:15:07 -0400

    refactor: Replace child clone logic in base file

---
+++ src/components/App.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+
+import Landing from "./landing";
+import JobList from "./joblist";
+import Contact from "./contact";
+
+const INTERSECTION_THRESHOLD = 0.6;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentBg: "newgray",
+      lastComponentId: "",
+      jlPage: null,
+    };
+
+    this.refsList = {
+      landing: React.createRef(),
+      joblist: React.createRef(),
+      contact: React.createRef(),
+    };
+
+    this.resolveBg = this.resolveBg.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    const observer = new IntersectionObserver(this.handleScroll, {
+      threshold: INTERSECTION_THRESHOLD,
+    });
+    Object.values(this.refsList).forEach((ref) =>
+      observer.observe(ref.current)
+    );
+  }
+
+  handleScroll(entries) {
+    entries.forEach((entry) => {
+      if (
+        entry.isIntersecting &&
+        Math.abs(INTERSECTION_THRESHOLD - entry.intersectionRatio) <= 0.1
+      ) {
+        this.resolveBg(
+          entry.target.id,
+          entry.target.id === "joblist" ? this.state.jlPage : null
+        );
+      }
+    });
+  }
+
+  resolveBg(componentId, jlPage = null) {
+    if (componentId !== this.state.lastComponentId || jlPage !== null) {
+      let resolvedBackground = "";
+
+      if (!jlPage) {
+        switch (componentId) {
+          case "joblist":
+            resolvedBackground = "indianred";
+            break;
+          case "contact":
+            resolvedBackground = "black";
+            break;
+          default:
+            resolvedBackground = "newgray";
+            break;
+        }
+      } else {
+        const bgMapping = ["engage", "wellpad", "crkf", "cdot"];
+
+        switch (bgMapping[jlPage]) {
+          case "engage":
+            resolvedBackground = "indianred";
+            break;
+          case "wellpad":
+            resolvedBackground = "mediumseagreen";
+            break;
+          case "crkf":
+            resolvedBackground = "darkslategrey";
+            break;
+          default:
+            resolvedBackground = "maroon";
+            break;
+        }
+      }
+
+      this.setState((currentState) =>
+        currentState.currentBg !== resolvedBackground
+          ? {
+              currentBg: resolvedBackground,
+              lastComponentId: componentId,
+              jlPage: jlPage !== null ? jlPage : currentState.jlPage,
+            }
+          : null
+      );
+    }
+  }
+
+  render() {
+    const baseProps = {
+      refsList: this.refsList,
+      currentBg: this.state.currentBg,
+      resolveBg: this.resolveBg,
+    };
+
+    return (
+      <div className={this.state.currentBg}>
+        <Landing {...baseProps} />
+        <JobList {...baseProps} />
+        <Contact {...baseProps} />
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Background from './components/background';
-import Landing from './components/landing';
-import JobList from './components/joblist';
-import Contact from './components/contact';
+import App from './components/App';
 
 import './stylesheets/main.scss'
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <Background>
-    <Landing />
-    <JobList />
-    <Contact />
-  </Background>,
+  <App />,
   document.getElementById('app')
 );
 
